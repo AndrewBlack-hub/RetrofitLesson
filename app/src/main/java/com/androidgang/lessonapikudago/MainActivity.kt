a2f5018e737734007214016db7aa7d828f8a8831
AndrewBlack-hub <crys2014@mail.ru>
2021-04-25 22:57:13 +0700

    use glide

--- app/src/main/java/com/androidgang/lessonapikudago/MainActivity.kt
+++ app/src/main/java/com/androidgang/lessonapikudago/MainActivity.kt
@@ -1,24 +1,67 @@
 package com.androidgang.lessonapikudago
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var titleMovies: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var progressBar: ProgressBar
+    private val url = "https://img1.fonwall.ru/o/xq/blue-flowers-stairs-plants-gate.jpeg"
+
+    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         titleMovies = findViewById(R.id.tv_title_list)
+        imageView = findViewById(R.id.iv_image)
+        progressBar = findViewById(R.id.pb_loader)
 
-        getMovies()
+        Glide.with(this)//передаем контекст (activity)
+            .load(url)//откуда загружаем
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+            })
+            .into(imageView)//куда вставляем(загружаем)
+
+        disposable = getMovies()
             .subscribeOn(Schedulers.io())//где выполняется (другой поток input/output)
             .observeOn(AndroidSchedulers.mainThread())//где получаем ответ (в главном потоке)
             .doOnNext { moviesResponse -> displayMovies(moviesResponse) }//выводит результат в лог
@@ -41,5 +84,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
         titleMovies.text = stringBuilder//готовые данные показываем в TextView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
     }
 }
